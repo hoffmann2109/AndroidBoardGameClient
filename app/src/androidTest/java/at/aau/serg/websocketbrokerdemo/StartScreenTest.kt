417b765eb8d009135eccbbb131019867830e3c70
chhandler <163014500+chhandler@users.noreply.github.com>
2025-04-04 19:09:47 +0200

    did refactoring of UI file placement to avoid conflicts later

--- app/src/androidTest/java/at/aau/serg/websocketbrokerdemo/StartScreenTest.kt
+++ app/src/androidTest/java/at/aau/serg/websocketbrokerdemo/StartScreenTest.kt
@@ -3,6 +3,7 @@ package at.aau.serg.websocketbrokerdemo
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import at.aau.serg.websocketbrokerdemo.ui.StartScreen
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
