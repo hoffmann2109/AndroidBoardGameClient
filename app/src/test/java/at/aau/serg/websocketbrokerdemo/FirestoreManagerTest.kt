20dcde73590f122b1552714268096102dbfd438a
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-04-05 06:02:13 +0200

    changed some imports and added a new branch to the workflow

--- app/src/test/java/at/aau/serg/websocketbrokerdemo/FirestoreManagerTest.kt
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/FirestoreManagerTest.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
 import org.mockito.Mockito.`when`
+import org.junit.jupiter.api.Assertions.*
 
 class FirestoreManagerTest {
 
