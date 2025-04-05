d18f03119e1fbc5d60c01361a0502df1d16f7785
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-04-05 07:42:31 +0200

    Restructured the build.gradle.kts and libs.versions.toml file

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/LobbyScreen.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/LobbyScreen.kt
@@ -38,7 +38,6 @@ fun LobbyScreen(
     onRollDice: () -> Unit,
     onLogout: () -> Unit,
     onProfileClick: () -> Unit,
-    playerProfile: PlayerProfile? = null
 ) {
     var showWifiIcon by remember { mutableStateOf(false) }
     var showDisconnectIcon by remember { mutableStateOf(false) }
@@ -203,7 +202,7 @@ fun AnimatedButton(text: String, color: Color, onClick: () -> Unit) {
 @Composable
 fun DiceRollingButton(text: String, color: Color, onClick: () -> Unit, diceResult: String) {
     var isPressed by remember { mutableStateOf(false) }
-    var rotateAngle by remember { mutableStateOf(0f) }
+    var rotateAngle by remember { mutableFloatStateOf(0f) }
 
     val rotation by animateFloatAsState(
         targetValue = rotateAngle,
--- app/src/test/java/at/aau/serg/websocketbrokerdemo/LobbyScreenTest.kt
+++ app/src/test/java/at/aau/serg/websocketbrokerdemo/LobbyScreenTest.kt
@@ -1,21 +1,14 @@
 package at.aau.serg.websocketbrokerdemo
 
-import android.content.Context
-import android.content.res.AssetManager
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import at.aau.serg.websocketbrokerdemo.ui.parseDiceResult
-import okhttp3.WebSocket
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.AfterEach
-import org.mockito.Mockito.*
-import java.io.ByteArrayInputStream
-import org.junit.jupiter.api.Assertions.*
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class LobbyScreenUITest {
+class LobbyScreenTest {
 
     @BeforeEach
     fun setup(){
@@ -24,20 +17,29 @@ class LobbyScreenUITest {
 
     @Test
     fun diceResultIsParsedCorrectly(){
-        // Case 1: Valid dice result is present
         val input1 = "Player rolled 4"
         val expected1 = "4"
         assertEquals(expected1, parseDiceResult(input1))
 
-        // Case 2: Valid dice result is present in a longer message
         val input2 = "Game update: Player rolled 6 and moved forward"
         val expected2 = "6"
         assertEquals(expected2, parseDiceResult(input2))
 
-        // Case 3: No dice result present, should return "?"
         val input3 = "No roll happened this round"
         val expected3 = "?"
         assertEquals(expected3, parseDiceResult(input3))
     }
+
+    @Test
+    fun NoDiceResultFoundDisplaysQuestionmark(){
+        val input1 = "Hello World!"
+        val expected1 = "?"
+        assertEquals(expected1, parseDiceResult(input1))
+        val input2 = ""
+        val expected2 = "?"
+        assertEquals(expected2, parseDiceResult(input2))
+    }
+
+
 
 }
