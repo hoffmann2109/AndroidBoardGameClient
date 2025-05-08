e90321e587c205f4d37f903a878e05e720cea002
chhandler <163014500+chhandler@users.noreply.github.com>
2025-05-08 22:19:55 +0200

    remove unused import

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/LeaderboardScreen.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/LeaderboardScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.MenuAnchorType.Companion.PrimaryNotEditable
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
