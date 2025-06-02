b048653a6ff597fe8d924bd7374c1a71434f5591
lendlerm <lendlerm@edu.htl-villach.at>
2025-06-02 17:31:36 +0200

    Removed unsued variable offered money

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/PlayboardScreen.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/PlayboardScreen.kt
@@ -164,7 +164,6 @@ fun PlayboardScreen(
         .toMap()
     var showDealDialog by remember { mutableStateOf(false) }
     var selectedReceiver by remember { mutableStateOf<PlayerMoney?>(null) }
-    var offeredMoney by remember { mutableStateOf(0) }
 
     var isCountering by remember { mutableStateOf(false) }
 
